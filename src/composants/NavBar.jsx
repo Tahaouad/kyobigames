83c2978323b69d67107a382064818ee2e2280c8f
Tahaouad <tahaouad04@gmail.com>
2024-02-03 01:22:46 +0100

    RWD

--- src/composants/NavBar.jsx
+++ src/composants/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
     return (
         <Navbar expand="lg" className="fixed-top" style={{ backgroundColor: '#000123' }}>
                 <Container className='w-100 mx-auto text-center'>
-                <Navbar.Toggle aria-controls="navbarNav" />
+                <Navbar.Toggle aria-controls="navbarNav" style={{color:'#FF0F7B',backgroundColor:'#FF0F7B'}}/>
                 <Navbar.Collapse id="navbarNav">
                     <Nav className="mx-auto">
                         <Nav.Link href="#" className='fw-bold fs-6' style={{ color: '#FF0F7B', fontFamily: 'Silkscreen' }}>Kyobi games</Nav.Link>
